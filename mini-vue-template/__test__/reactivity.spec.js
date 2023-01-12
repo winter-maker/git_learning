23d9421a0f40ccade7257a371a7bdfb1e5c49c34
lyjlw <985289299@qq.com>
2023-01-12 22:28:43 +0800

    数据响应式

--- mini-vue-template/__test__/reactivity.spec.js
+++ mini-vue-template/__test__/reactivity.spec.js
@@ -37,18 +37,18 @@ describe("reacticity", () => {
     expect(dummy).toBe(21);
   });
 
-  // it("should handle multiple effects", () => {
-  //   let dummy1, dummy2;
-  //   const counter = reactive({ num: 0 });
-  //   effect(() => (dummy1 = counter.num));
-  //   effect(() => (dummy2 = counter.num));
+  it("should handle multiple effects", () => {
+    let dummy1, dummy2;
+    const counter = reactive({ num: 0 });
+    effect(() => (dummy1 = counter.num));
+    effect(() => (dummy2 = counter.num));
 
-  //   expect(dummy1).toBe(0);
-  //   expect(dummy2).toBe(0);
-  //   counter.num++;
-  //   expect(dummy1).toBe(1);
-  //   expect(dummy2).toBe(1);
-  // });
+    expect(dummy1).toBe(0);
+    expect(dummy2).toBe(0);
+    counter.num++;
+    expect(dummy1).toBe(1);
+    expect(dummy2).toBe(1);
+  });
   it("effect should linked to the exact key", () => {
     const observe = reactive({ foo: "foo", bar: "bar" });
     const fnSpy = jest.fn(() => {
@@ -60,42 +60,44 @@ describe("reacticity", () => {
     observe.foo = "foooo";
     expect(fnSpy).toHaveBeenCalledTimes(2);
   });
-  // it("调度执行", () => {
-  //   const obj = reactive({ foo: 1 });
-  //   const arr = [];
-  //   jest.useFakeTimers(); // 开启模拟定时器
-  //   effect(() => arr.push(obj.foo), {
-  //     // 调度器
-  //     scheduler(fn) {
-  //       setTimeout(fn);
-  //     },
-  //   });
-  //   obj.foo++;
-  //   arr.push("over");
+  //调度执行，异步更新视图，形成异步更新队列
+  it("调度执行", () => {
+    const obj = reactive({ foo: 1 });
+    const arr = [];
+    jest.useFakeTimers(); // 开启模拟定时器
+    effect(() => arr.push(obj.foo), {
+      // 调度器
+      scheduler(fn) {
+        setTimeout(fn);
+      },
+    });
+    obj.foo++;
+    arr.push("over");
 
-  //   jest.runAllTimers(); // 等待所有定时器执行
-  //   expect(arr[0]).toBe(1);
-  //   expect(arr[1]).toBe("over");
-  //   expect(arr[2]).toBe(2);
-  // });
-});
-it("should be nested", () => {
-  const obj = reactive({ foo: "foo", bar: "bar" });
-  const arr = [];
-  const mockFn1 = jest.fn(() => {
-    arr.push(obj.bar);
+    jest.runAllTimers(); // 等待所有定时器执行
+    expect(arr[0]).toBe(1);
+    expect(arr[1]).toBe("over");
+    expect(arr[2]).toBe(2);
   });
-  const mockFn2 = jest.fn(() => {
-    // mockFn1嵌套在mockFn2内部，obj.bar -> mockFn1
-    effect(mockFn1);
-    arr.push(obj.foo);
+  it("should be nested", () => {
+    const obj = reactive({ foo: "foo", bar: "bar" });
+    const arr = [];
+    const mockFn1 = jest.fn(() => {
+      arr.push(obj.bar);
+    });
+    const mockFn2 = jest.fn(() => {
+      // mockFn1嵌套在mockFn2内部，obj.bar -> mockFn1
+      effect(mockFn1);
+      arr.push(obj.foo);
+    });
+    // mockFn2包含了mockFn1，obj.foo -> mockFn2
+    effect(mockFn2);
+    // 因此希望内部mockFn1先执行，arr[0]是bar，arr[1]是foo
+    expect(arr[0]).toBe("bar");
+    expect(arr[1]).toBe("foo");
+    // 修改foo，希望触发mockFn2，再次添加obj.foo
+    obj.foo = "fooooo";
+    expect(arr[2]).toBe("bar");
+    expect(arr[3]).toBe("fooooo");
   });
-  // mockFn2包含了mockFn1，obj.foo -> mockFn2
-  effect(mockFn2);
-  // 因此希望内部mockFn1先执行，arr[0]是bar，arr[1]是foo
-  expect(arr[0]).toBe("bar");
-  expect(arr[1]).toBe("foo");
-  // 修改foo，希望触发mockFn2，再次添加obj.foo
-  obj.foo = "fooooo";
-  expect(arr[3]).toBe("fooooo");
 });
--- mini-vue-template/src/reactivity/reactive.js
+++ mini-vue-template/src/reactivity/reactive.js
@@ -1,15 +1,22 @@
-import { track, trigger } from "./effect";
+import { track, trigger, activeEffect } from "./effect";
+/**
+ * @title 将params对象变成响应式
+ * @param {*} params
+ * @returns
+ */
 export function reactive(params) {
   const proxyObj = new Proxy(params, {
     get(target, property, receiver) {
       const result = Reflect.get(target, property, receiver);
-      //收集target属性
+      //收集target属性，建立 target 、property 和 activeEffect之间的关系
       track(target, property);
       return result;
     },
     set(target, propKey, value, receiver) {
       const result = Reflect.set(target, propKey, value, receiver);
+      //触发target属性
       trigger(target, propKey);
+      list.forEach((fn) => fn());
       return result;
     },
     deleteProperty(target, property) {
