dd9a1ab282bea2a13d14c460bbfc879f7d90c440
lyjlw <985289299@qq.com>
2023-03-06 20:51:21 +0800

    http-state

--- CREATE_JS/stateMachine/http_state_machine.js
+++ CREATE_JS/stateMachine/http_state_machine.js
@@ -1,59 +1,79 @@
 const response = {
+    httpVersion: '',
     statusCode: '',
     statusText: '',
-    headers: {
-        ContentType: 'text/html'
-    },
+    headers: {},
     body: `<h1>Hello World</h1>`
 }
+let currentHeadKey = '';
+let currentHeadValue = '';
 module.exports = function parse(source) {
+    const EOF = Symbol('EOF')
     console.log(source)
     let state = start;
-    const EOF = Symbol('EOF')
+    
     for (let c of source.split('').concat(EOF)) {
         //console.log(state.name, c);
         state = state(c);
     }
     console.log('---response---', response)
-    // if (state === success) {
-    //     return true;
-    // }
-    // return false;
 }
-// afterHttp(char) reconsume
+// getStatusCode(char) reconsume
 
 function start(char) {
-    console.log('---start---', char)
-    if(char === ' ') return afterHttp;
+    //console.log('---start---', char)
+    if(char === ' ') return getStatusCode;
+    response.httpVersion += char;
     return start;
 }
 function success() {
     return success;
 }
-function afterHttp(char) {
+function getStatusCode(char) {
     if(char === ' ') {
-        return afterStatusCode;
+        return getStatusText;
     } else {
         response.statusCode += char; //写时的副作用
-        //console.log('---afterHttp---', char) //console.log 本身就是副作用
-        return afterHttp;
+        //console.log('---getStatusCode---', char) //console.log 本身就是副作用
+        return getStatusCode;
     }
 }
-function afterStatusCode(char) {
+function getStatusText(char) {
     if(char === '\r') {
-        
-        return afterStatusText
+        return clearlineN;
     } else {
         response.statusText += char;
-        //console.log('--afterStatusCode--', char)//console.log 本身就是副作用
-        return afterStatusCode
+        //console.log('--getStatusText--', char)//console.log 本身就是副作用
+        return getStatusText
     }
     
 }
-function afterStatusText(char) {
-    //console.log('--afterStatusText--',char)
-    return afterStatusText
+function clearlineN(char) {
+    if(char === '\n') return getHeadersKey
+    return error;
 }
+function getHeadersKey(char) {
+    if(char === ':') return afterHeadersValue;
+    currentHeadKey += char;
+    return getHeadersKey;
+}
+function afterHeadersValue(char) {
+    if(char !== '') return getHeadersValue;
+    return afterHeadersValue;
+}
+function getHeadersValue(char) {
+    if(char === '\r') {
+        response.headers[currentHeadKey] = currentHeadValue;
+        currentHeadKey = '';
+        currentHeadValue = '';
+        return clearlineN;
+    }
+    currentHeadValue += char;
+    return getHeadersValue;
+}
+
+function error(){ return error };
+
 /**
  * 思考：状态函数是不是纯函数
  * 结论：状态函数不是纯函数。状态函数是可以有副作用的，副作用必须是写时的副作用，不能从外面读东西，
