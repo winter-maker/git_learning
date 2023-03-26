9000d0edfda6f98f7a4c56a8d4f352bb8a353d88
lyjlw <985289299@qq.com>
2023-03-26 22:44:14 +0800

    mock测试

--- engineering/jest/src/__test__/delay.spec.js
+++ engineering/jest/src/__test__/delay.spec.js
@@ -4,12 +4,20 @@ const delay = require('../delay');
 describe('异步测试', ()=>{
     
     it('fn', done=>{
+        //间谍函数，
+        const mockFn = jest.fn();
+
         //模拟时间函数，让时间函数的速度进行快进的处理
         jest.useFakeTimers()
         delay(()=>{
+            mockFn(1)
+            mockFn(2)
             done()
         })
         jest.runAllTimers()
         expect(true).toBe(true)
+        expect(mockFn).toBeCalled();
+        expect(mockFn).toBeCalledTimes(2);
+        expect(mockFn).toHaveBeenCalledWith(1)
     })
 })
